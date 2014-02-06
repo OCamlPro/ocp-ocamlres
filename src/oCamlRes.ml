b7e9d714696e81a62c5b2c03ce9636455a78fd8b
Benjamin Canou <benjamin@canou.fr>
2014-02-06 13:51:06 +0100

    License headers update

--- src/oCamlRes.ml
+++ src/oCamlRes.ml
@@ -1,19 +1,17 @@
-(* This file is part of ocp-ocamlres - directory scanning
+(* This file is part of ocp-ocamlres - main library
  * (C) 2013 OCamlPro - Benjamin CANOU
  *
  * ocp-ocamlres is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
+ * version 3.0 of the License, or (at your option) any later
+ * version, with linking exception.
  * 
  * ocp-ocamlres is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with ocp-ocamlres.
- * If not, see <http://www.gnu.org/licenses/>. *)
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the LICENSE file for more details *)
  
 (** Paths inside resource stores. *)
 module Path = struct
